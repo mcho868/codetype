f5a2934519b2a9d211528cfcbd408ece2a260769
mco868 <mcho868@aucklanduni.ac.nz>
2026-07-29 01:48:53 +1200

    deleted files

--- error.py
+++
@@ -1,9 +0,0 @@
-def f(i):
-    if type(i) != int:
-        raise ValueError("this is not an integer")
-
-    return f"this is an integer: {int(i)}"
-
-
-print(f(1))
-print(f("dsa"))
--- p.py
+++
@@ -1,3 +0,0 @@
-print(1 // 4)
-print(4 // 4)
-print(5 // 4)
